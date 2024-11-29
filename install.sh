3542ac8754d07e60a6dda64d868f1f1fd03df25b
Antoine Brun <antoine.brun@gmail.com>
2024-11-29 17:20:51 +0100

    add installer script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. /usr/share/install-libraries/il-lib.sh
+
+pushd /opt/fmc_repository/Automation || exit
+
+emit_step "Automation from OpenMSA_Automation"
+ln -fsn ../OpenMSA_Automation/Catalog Catalog;
+emit_step "CLI tools (TF, kubectl, AWS CLI, Google SDK...)"
+popd || exit
+pushd /opt/fmc_repository/Process/PythonReference || exit
+emit_step "Symlink to CCLA_WF"
+ln -fsn /opt/fmc_repository/Process/cloudclapp-wf/cloud_sdk ccla_cloud
+popd || exit

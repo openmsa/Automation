878d093f6c02766a22f4b983105c3b1d78758f05
Antoine Brun <antoine.brun@gmail.com>
2025-04-01 14:58:15 +0200

    update packaging / Dockerfile

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ pushd /opt/fmc_repository/Automation || exit
 
 emit_step "Automation from OpenMSA_Automation"
 ln -fsn ../OpenMSA_Automation/Catalog Catalog;
+chown -R ncuser.ncuser /opt/fmc_repository/OpenMSA_Automation/
 popd || exit
 pushd /opt/fmc_repository/Process/PythonReference || exit
 emit_step "Symlink to CCLA_WF"
